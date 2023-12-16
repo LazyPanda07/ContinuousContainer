1ad3678f76a17f3782b74a7ccc1f1a6a1b454556
Semyon Gritsenko <40473939+LazyPanda07@users.noreply.github.com>
2023-12-16 15:23:15 +0500

    Refactor

---
+++ Tests/src/ContinuousContainerTest.cpp
@@ -0,0 +1,13 @@
+#include "gtest/gtest.h"
+
+#include "ContinuousContainer.h"
+
+TEST(ContinuousContainer, Add)
+{
+    containers::ContinuousContainer container;
+}
+
+TEST(ContinuousContainer, Getters)
+{
+    containers::ContinuousContainer container;
+}
